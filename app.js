1358ae8ce23e88f49060637d336406bd339afc79
Nicole Lorena Rojas Ramirez <nicole.rojas@genstudents.org>
2024-06-11 21:11:11 -0500

    Entrega portafolio Generation

--- app.js
+++ app.js
@@ -1,3 +1,186 @@
+
+//-------------- Inicio cambio de modo claro/oscuro -------------- 
+
+// TODO completar la funcion para que cambien los colores de los boxes y la letra. No se veían tan bien los colores pastel claro; el tiempo no me dio para mirar otros.
+const modeBoxArray = [
+  {
+    element: document.body,
+    lightColor: '#e7f2f8',
+    darkColor: '#1f1f1f',
+  },
+  // {
+  //   element: document.getElementsByClassName('welcome')[0],
+  //   lightColor: '#96dcff',
+  //   darkColor: '#66ccff',
+  // },
+  // {
+  //   element: document.getElementsByClassName('language')[0],
+  //   lightColor: '#fdeea3',
+  //   darkColor: '#f5dd63',
+  // },
+  // {
+  //   element: document.getElementsByClassName('toggle-lang')[0],
+  //   lightColor: '#fa9ea7',
+  //   darkColor: '#e64553',
+  // },
+  // {
+  //   element: document.getElementsByClassName('mode')[0],
+  //   lightColor: '#949090',
+  //   darkColor: '#4e4e4e',
+  // },
+  // {
+  //   element: document.getElementsByClassName('links')[0],
+  //   lightColor: '#f5d8e8',
+  //   darkColor: '#f6b2d7',
+  // },
+  // {
+  //   element: document.getElementsByClassName('about')[0],
+  //   lightColor: '#cdb7f8',
+  //   darkColor: '#9966ff',
+  // },
+  // {
+  //   element: document.getElementsByClassName('skills')[0],
+  //   lightColor: '#fa9ea7',
+  //   darkColor: '#FA6977',
+  // },
+  // {
+  //   element: document.getElementsByClassName('learning')[0],
+  //   lightColor: '#fdeea3',
+  //   darkColor: '#ffe666',
+  // },
+]
+
+// const modeContentArray = [  
+//   {
+//     element: document.getElementsByClassName('language')[0].children[0],
+//     lightColor: '#fa9ea7',
+//     darkColor: '#e64553',
+//   },
+//   {
+//     element: document.getElementsByClassName('skills')[0].children[0],
+//     lightColor: '#f33448',
+//     darkColor: 'white',
+//   },
+//   {
+//     element: document.getElementsByClassName('learning')[0].children[0],
+//     lightColor: '#1F1F1F',
+//     darkColor: 'white',
+//   },
+// ]
+
+let pageMode = "dark";
+const modeBox = document.getElementsByClassName('mode')[0];
+modeBox.addEventListener('click', changeMode);
+function changeMode(){
+  for(let item of modeBoxArray){
+    if(pageMode === "dark"){
+      item.element.style.backgroundColor = item.lightColor;
+    }else{
+      item.element.style.backgroundColor = item.darkColor;
+    }
+  }
+  // for(let item of modeContentArray){
+  //   console.log(item)
+  //   if(pageMode === "dark"){
+  //     item.element.style.color = item.lightColor;
+  //   }else{
+  //     item.element.style.color = item.darkColor;
+  //   }
+  // }
+  if(pageMode === "dark"){
+    pageMode = "light";
+  }else{
+    pageMode = "dark"
+  }
+
+  changeModeImage();
+}
+
+
+function changeModeImage(){
+  if(pageMode === "dark"){
+    modeBox.style.backgroundImage = "url('./sun.gif')";
+    modeBox.style.backgroundColor = "#fffefe";
+
+  }
+  else{
+    modeBox.style.backgroundImage = "url('./moon.gif')";
+    modeBox.style.backgroundColor = "#white";
+  }
+}
+// -------------- Final cambio de modo claro/oscuro-------------- //
+
+
+// TODO funcion js traducción
+
+
+// --------------Inicio función para animar los scrolls --------------//
+// TODO falta incluir el icon pause
+
+let isMoving1 = false;
+let isMoving2 = false;
+
+const skillsButton = document.querySelector(".playbutton1");
+skillsButton.addEventListener("click", playPauseScroll1)
+const scrolls1 = document.querySelectorAll(".scroll-content-1");
+
+const learningButton = document.querySelector(".playbutton2");
+learningButton.addEventListener("click", playPauseScroll2)
+const scrolls2 = document.querySelectorAll(".scroll-content-2");
+
+
+function playPauseScroll1 (){
+  scrolls1.forEach((element) => {
+    if (isMoving1){
+      element.style.animationPlayState = "paused";
+    }
+    else{
+      element.style.animationPlayState = "running";
+    }
+  })
+  isMoving1 = !isMoving1;
+}
+function playPauseScroll2 (){
+  scrolls2.forEach((element) => {
+    if (isMoving2){
+      element.style.animationPlayState = "paused";
+    }
+    else{
+      element.style.animationPlayState = "running";
+    }
+  })
+  isMoving2 = !isMoving2;
+}
+
+// --------------Fin función para animar los scrolls --------------//
+
+// TODO función js para validar formulario. 
+// *Ojo, en css hay un desajuste en desktop con la posicion de las etiquetas.
+
+// INICIO Cambio de posición de los labels del formulario cuando hay texto al interior de un input
+// const emailLabel = document.querySelector('label[for="email"]');
+// const emailInput = document.getElementById('email');
+// emailInput.addEventListener('blur', function() {
+//   if (emailInput.value.trim() !== "") {
+//     emailLabel.style.top = '7px';
+//   }else{
+//     if(emailInput !== document.activeElement){
+//       emailLabel.style.top = '50%';
+//     }else{
+//       emailLabel.style.top = '7px';
+//     }
+//   }
+// });
+
+// emailInput.addEventListener('focus', function() {
+//   emailLabel.style.top = '7px';
+// });
+// FIN Cambio de posición de los labels del formulario cuando hay texto al interior de un input
+
+
+
+// ----------INICIO SCROLL UP BUTTON ----------//
+
 upButton= document.querySelector('.scroll-up')
 upButton.addEventListener('click', function() {
 window.scrollTo({
@@ -6,344 +189,12 @@ window.scrollTo({
   });
 });
 
-
-
-
-// @media only screen and (min-width: 768px) {
-//   .container{
-//     min-width: 910px;
-//   }
- 
-// }
-// @media only screen and (min-width: 768px) {
- 
-
-// .container {
-//   display: grid;
-//   grid-template-columns: repeat(12, minmax(0, 1fr));
-//   grid-template-rows: repeat(12, 1fr);
-//   gap:1em;
-//   padding: 2em;
-//   max-width: 1300px;
-// }
-// /* welcome */
-// .box:nth-child(1){ grid-column: span 7; height: 33em;}
-// /* links */
-// .box:nth-child(4){ grid-row: span 1; order:1; padding: 10px;}
-// .links div{
-//   display: flex;
-//   flex-direction: column;
-//   justify-content:space-between;
-//   align-items: center;
-//   width: 100%;
-//   height: 100%;
-// }
-// .links img{
-//   max-width: 30%;
-//   min-width: 46px;
-// }
-// /* language */
-// .box:nth-child(2){ grid-column: span 1;  order:3;}
-// /* mode */
-// .box:nth-child(3){ order:4;}
-// /* about */
-// .box:nth-child(5){ grid-column: span 5;  grid-row: span 3; order: 5;}
-// /* cv */
-// .box:nth-child(9){order: 6;}
-// /* skills */
-// .box:nth-child(6){ grid-column: span 9; order: 7}
-// /* learning */
-// .box:nth-child(7){ grid-column: span 9; order: 8}
-// /* map */
-// .box:nth-child(13){ grid-column: span 5; grid-row: span 2; order:9;}
-// .map img{
-//   max-width: 20%;
-// }
-// .map p{
-//   color: #1f1f1f;
-//   font-size: 1.1em;
-//   font-weight: 700;
-//   background-color: rgba(255, 255, 255, 0.8);
-//   text-shadow: 2px 2px 4px rgba(255, 255, 255, 0.7);
-//   padding: 1px 10px;
-// }
-// /* age */
-// .box:nth-child(11){ grid-column: span 3; grid-row: span 1; order:10;}
-// /* english */
-// .box:nth-child(8){ grid-column: span 2; grid-row: span 2; order:11;}
-// /* japanese */
-// .box:nth-child(10){ grid-column: span 2; grid-row: span 2; order:12;}
-// /* counter */
-// .box:nth-child(12){ grid-column: span 3; grid-row: span 1; order:13;}
-// /* projects */
-// .box:nth-child(14){order:14;}
-// /* contact */
-// .box:nth-child(15){ grid-column: span 9; grid-row: span 3; order:15;}
-// /* footer */
-// .box:nth-child(16){ grid-column: span 12; order:16;}
-// }
+// ----------FIN SCROLL UP BUTTON ----------//
 
 
 
 
 
 
-// .sakura {
-// 	position: absolute;
-// 	width: 100%;
-// 	height: 100%;
-// }
-// .sakura span {
-// 	position: absolute;
-// 	top: -10%;
-// 	left: 0;
-// 	width: 0.8vw;
-// 	height: 0.4vw;
-// 	border-radius: 5% 80% 10% 80%;
-// 	background-color: #ffb6c1;
-// }
-// /* add animation */
-// .sakura span:nth-child(2n+1) {
-// 	animation: sakura-anim-1 10s 0s linear infinite;
-// }
-// .sakura span:nth-child(2n+2) {
-// 	animation: sakura-anim-2 10s 0s linear infinite;
-// }
-// /* side position */
-// .sakura span:nth-child(1) {
-// 	left: 0%;
-// }
-// .sakura span:nth-child(2) {
-// 	left: 2%;
-// }
-// .sakura span:nth-child(3) {
-// 	left: 4%;
-// }
-// .sakura span:nth-child(4) {
-// 	left: 6%;
-// }
-// .sakura span:nth-child(5) {
-// 	left: 8%;
-// }
-// .sakura span:nth-child(6) {
-// 	left: 10%;
-// }
-// .sakura span:nth-child(7) {
-// 	left: 12%;
-// }
-// .sakura span:nth-child(8) {
-// 	left: 14%;
-// }
-// .sakura span:nth-child(9) {
-// 	left: 16%;
-// }
-// .sakura span:nth-child(10) {
-// 	left: 18%;
-// }
-// .sakura span:nth-child(11) {
-// 	left: 20%;
-// }
-// .sakura span:nth-child(12) {
-// 	left: 22%;
-// }
-// .sakura span:nth-child(13) {
-// 	left: 24%;
-// }
-// .sakura span:nth-child(14) {
-// 	left: 26%;
-// }
-// .sakura span:nth-child(15) {
-// 	left: 28%;
-// }
-// .sakura span:nth-child(16) {
-// 	left: 30%;
-// }
-// .sakura span:nth-child(17) {
-// 	left: 32%;
-// }
-// .sakura span:nth-child(18) {
-// 	left: 34%;
-// }
-// .sakura span:nth-child(19) {
-// 	left: 36%;
-// }
-// .sakura span:nth-child(20) {
-// 	left: 38%;
-// }
-// .sakura span:nth-child(21) {
-// 	left: 40%;
-// }
-// .sakura span:nth-child(22) {
-// 	left: 42%;
-// }
-// .sakura span:nth-child(23) {
-// 	left: 44%;
-// }
-// .sakura span:nth-child(24) {
-// 	left: 46%;
-// }
-// .sakura span:nth-child(25) {
-// 	left: 48%;
-// }
-// .sakura span:nth-child(26) {
-// 	left: 50%;
-// }
-
-// .sakura span:nth-child(27) {
-// 	left: 52%;
-// }
-// .sakura span:nth-child(28) {
-// 	left: 54%;
-// }
-// .sakura span:nth-child(29) {
-// 	left: 56%;
-// }
-// .sakura span:nth-child(30) {
-// 	left: 58%;
-// }
-// .sakura span:nth-child(31) {
-// 	left: 60%;
-// }
-// .sakura span:nth-child(32) {
-// 	left: 62%;
-// }
-// .sakura span:nth-child(33) {
-// 	left: 64%;
-// }
-// .sakura span:nth-child(34) {
-// 	left: 66%;
-// }
-// .sakura span:nth-child(35) {
-// 	left: 68%;
-// }
-// .sakura span:nth-child(36) {
-// 	left: 70%;
-// }
-// .sakura span:nth-child(37) {
-// 	left: 72%;
-// }
-// .sakura span:nth-child(38) {
-// 	left: 74%;
-// }
-// .sakura span:nth-child(39) {
-// 	left: 76%;
-// }
-// .sakura span:nth-child(40) {
-// 	left: 78%;
-// }
-// .sakura span:nth-child(41) {
-// 	left: 80%;
-// }
-// .sakura span:nth-child(42) {
-// 	left: 82%;
-// }
-// .sakura span:nth-child(43) {
-// 	left: 84%;
-// }
-// .sakura span:nth-child(44) {
-// 	left: 86%;
-// }
-// .sakura span:nth-child(45) {
-// 	left: 88%;
-// }
-// .sakura span:nth-child(46) {
-// 	left: 90%;
-// }
-// .sakura span:nth-child(47) {
-// 	left: 92%;
-// }
-// .sakura span:nth-child(48) {
-// 	left: 94%;
-// }
-// .sakura span:nth-child(49) {
-// 	left: 96%;
-// }
-// .sakura span:nth-child(50) {
-// 	left: 98%;
-// }
-// /* size */
-// .sakura span:nth-child(3n+1) {
-// 	width: 1vw;
-// 	height: 0.5vw;
-// }
-// .sakura span:nth-child(3n+2) {
-// 	width: 0.8vw;
-// 	height: 0.4vw;
-// }
-// .sakura span:nth-child(3n+3) {
-// 	width: 0.6vw;
-// 	height: 0.3vw;
-// }
-// /* animation-duration */
-// .sakura span:nth-child(4n+1) {
-// 	animation-duration: 5s;
-// }
-// .sakura span:nth-child(4n+2) {
-// 	animation-duration: 12s;
-// }
-// .sakura span:nth-child(4n+3) {
-// 	animation-duration: 8s;
-// }
-// .sakura span:nth-child(4n+4) {
-// 	animation-duration: 6s;
-// }
-// /* animation-delay */
-// .sakura span:nth-child(11n+1) {
-// 	animation-delay: 0s;
-// }
-// .sakura span:nth-child(11n+2) {
-// 	animation-delay: 9s;
-// }
-// .sakura span:nth-child(11n+3) {
-// 	animation-delay: 2s;
-// }
-// .sakura span:nth-child(11n+4) {
-// 	animation-delay: 5s;
-// }
-// .sakura span:nth-child(11n+5) {
-// 	animation-delay: 6s;
-// }
-// .sakura span:nth-child(11n+6) {
-// 	animation-delay: 7s;
-// }
-// .sakura span:nth-child(11n+7) {
-// 	animation-delay: 3s;
-// }
-// .sakura span:nth-child(11n+8) {
-// 	animation-delay: 1s;
-// }
-// .sakura span:nth-child(11n+9) {
-// 	animation-delay: 2s;
-// }
-// .sakura span:nth-child(11n+10) {
-// 	animation-delay: 11s;
-// }
-// .sakura span:nth-child(11n+11) {
-// 	animation-delay: 10s;
-// }
-// /* animation */
-// @keyframes sakura-anim-1 {
-// 	0% {
-// 		top: -10%;
-// 		transform: translateX(0) rotateX(0) rotateY(0);
-// 	}
-// 	100% {
-// 		top: 100%;
-// 		transform: translateX(15vw) rotateX(180deg) rotateY(360deg);
-// 	}
-// }
-// @keyframes sakura-anim-2 {
-// 	0% {
-// 		top: -10%;
-// 		transform: translateX(0);
-// 	}
-// 	100% {
-// 		top: 100%;
-// 		transform: translateX(-15vw) rotateX(180deg) rotateY(360deg);
-// 	}
-// }
 
 
-// Inicio contador de días para el cumple
-
-// Final contador de días para el cumple
